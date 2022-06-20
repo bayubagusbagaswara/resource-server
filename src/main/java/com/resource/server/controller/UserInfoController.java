b05962f4cd255f0057f2a433355500233b35f107
Bayu Bagus Bagaswara <bagaszwara12@gmail.com>
2022-06-20 13:30:25 +0700

    resource server

---
+++ src/main/java/com/resource/server/controller/UserInfoController.java
@@ -0,0 +1,31 @@
+package com.resource.server.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class UserInfoController {
+
+    @GetMapping("/user/info")
+    public Map<String, Object> userInfo(Authentication currentUser) {
+        Map<String, Object> hasil = new HashMap<>();
+
+        hasil.put("waktu", LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        hasil.put("authentication class", currentUser.getClass().getSimpleName());
+        // ambil data user yang sedang login
+        hasil.put("current authentication", currentUser);
+
+        JwtAuthenticationToken currentAuthentication = (JwtAuthenticationToken) currentUser;
+        currentAuthentication.getName();
+
+        return hasil;
+    }
+}
